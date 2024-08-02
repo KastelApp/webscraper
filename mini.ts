c62002a75e355c26bf051937a4414eb8d7221657
Darker-Ink <rstonehouse66@gmail.com>
2024-08-02 03:13:52 -0500

    feat: possible fix

--- mini.ts
+++ mini.ts
@@ -6,6 +6,7 @@ const mf = new Miniflare({
     compatibilityDate: "2024-07-29",
     compatibilityFlags: ["nodejs_compat"],
     port: 8080,
+    host: "0.0.0.0",
     workers: [{
         name: "worker",
         modules: [{
