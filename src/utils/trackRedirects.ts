c2b37dd7ecf99f126cc5631c5fabc20f6b934eb0
Darker-Ink <rstonehouse66@gmail.com>
2024-08-18 20:32:34 -0500

    chore: some misc stuff

--- src/utils/trackRedirects.ts
+++ src/utils/trackRedirects.ts
@@ -14,19 +14,28 @@ const trackRedirects = async (
 	const visitedUrls: string[] = [];
 
 	while (currentUrl) {
-		const signal = new AbortController();
-
-		setTimeout(() => {
-			signal.abort();
-		}, 8000);
-
 		visitedUrls.push(currentUrl);
 
 		const [response, error] = await promiseHandler(
 			fetch(currentUrl, {
 				method: method,
 				redirect: "manual",
-				signal: signal.signal,
+				headers: {
+					"User-Agent":
+						"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+					accept:
+						"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+					"accept-language": "en-US,en;q=0.9",
+					priority: "u=0, i",
+					"sec-ch-ua": '"Not)A;Brand";v="99", "Microsoft Edge";v="127", "Chromium";v="127"',
+					"sec-ch-ua-mobile": "?0",
+					"sec-ch-ua-platform": '"Windows"',
+					"sec-fetch-dest": "document",
+					"sec-fetch-mode": "navigate",
+					"sec-fetch-site": "none",
+					"sec-fetch-user": "?1",
+					"upgrade-insecure-requests": "1",
+				},
 			}),
 		);
 
